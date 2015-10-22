821b2f9dbcef3d8d32b49aa2992fff9c57bfefab
kernelvinux <p.torpoco.ll@gmail.com>
2015-10-21 21:08:09 -0500

    Completed question 3 Midterm Exam

---
+++ Assembler/PC2-2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PC2-2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PC2-2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pc2-2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pc2-2.x.tar
